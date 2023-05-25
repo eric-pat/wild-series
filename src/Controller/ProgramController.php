fe5e63d6204fded78356e88aaff9c5b56dae0f0e
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-05-25 11:51:15 +0200

    change quetes 12

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -29,17 +29,11 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    #[Route('/show/{id}', name: 'show')]
     public function show(int $id, ProgramRepository $programRepository):Response
     {
-        $program = $programRepository->findOneBy(['id' => $id]);
-        // same as $program = $programRepository->find($id);
-
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
-            );
-        }
+        $program = $programRepository->find($id);
+        
         return $this->render('program/show.html.twig', [
             'program' => $program,
         ]);
