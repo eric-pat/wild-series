c7d76fb06ea943de4a3155dac0df0a332e5bced0
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-05-26 21:12:12 +0200

    end quetes 19

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -18,7 +20,15 @@ class ProgramType extends AbstractType
             ->add('title', TextType::class)
             ->add('synopsis', TextType::class)
             ->add('year', NumberType::class)
-            ->add('category', null, ['choice_label'=> 'name'])
+            ->add('category', null, [
+                'choice_label'=> 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' =>true,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
             ->add('poster', FileType::class, ['mapped' => false])
         ;
     }
